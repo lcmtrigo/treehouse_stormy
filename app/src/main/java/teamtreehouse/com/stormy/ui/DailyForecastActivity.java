7d39c9b767a93026893b7966ccbde45639bfe739
rmtrigo <rmontrigo@gmail.com>
2015-07-21 23:33:46 -0700

    Commit 1: Earned Standard ListViews badge

---
+++ app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
@@ -0,0 +1,23 @@
+package teamtreehouse.com.stormy.ui;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import teamtreehouse.com.stormy.R;
+
+public class DailyForecastActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+
+        String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+                "Friday", "Saturday" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, daysOfTheWeek);
+        setListAdapter(adapter);
+    }
+
+}
